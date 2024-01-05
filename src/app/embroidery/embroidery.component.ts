5a9b1133d741e350b4a2fd01917a253203dcb048
Israajddn <Israajddn@gmail.com>
2024-01-05 13:46:30 -0500

    created a second component

---
+++ src/app/embroidery/embroidery.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-embroidery',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      embroidery works!
+    </p>
+  `,
+  styleUrl: './embroidery.component.css'
+})
+export class EmbroideryComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { EmbroideryComponent } from '../embroidery/embroidery.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [EmbroideryComponent],
   template: `
      <section>
     <form>
       <input type="text" placeholder="Filter by price">
       <button class="primary" type="button">Search</button>
     </form>
+    </section>
+  <section class="results">
+    <app-embroidery></app-embroidery>
   </section>
   `,
   styleUrl: './home.component.css'
