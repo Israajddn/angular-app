87ba641501b236c8e8d97c7eaeb86b9f0407bbcf
Israajddn <Israajddn@gmail.com>
2024-01-06 13:55:04 -0500

    Integrate details page into application

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Embroidery } from '../embroidery';
+import { EmbroiderysService } from '../embroiderys.service';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      details works!
-    </p>
+   <article>
+    <img class="listing-photo" [src]="embroidery?.photo"
+      alt="Exterior photo of {{embroidery?.name}}"/>
+    <section class="listing-description">
+      <h2 class="listing-heading">{{embroidery?.name}}</h2>
+      <p class="listing-price">{{embroidery?.price}}</p>
+    </section>
+    <section class="listing-features">
+      <h2 class="section-heading">About this embroidery</h2>
+      <ul>
+        <li>color: {{embroidery?.color}}</li>
+        <li>Description: {{embroidery?.description}}</li>
+      </ul>
+    </section>
+  </article>
   `,
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  embroiderysService = inject(EmbroiderysService);
+  embroidery: Embroidery | undefined;
+
+  constructor() {
+    const embroideryId = Number(this.route.snapshot.params['id']);
+    this.embroidery = this.embroiderysService.getEmbroideryPricesById(embroideryId);
+  }
 
 }
--- src/app/embroidery/embroidery.component.ts
+++ src/app/embroidery/embroidery.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Embroidery } from '../embroidery';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-embroidery',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule,
+    RouterModule],
   template: `
    <section class="listing">
     <img class="listing-photo" [src]="embroidery.photo" alt="Exterior photo of {{embroidery.name}}">
     <h2 class="listing-heading">{{ embroidery.name }}</h2>
     <p class="listing-price">{{ embroidery.price}}, {{embroidery.available }}</p>
+    <a [routerLink]="['/details', embroidery.id]">Learn More</a>
   </section>
   `,
   styleUrl: './embroidery.component.css'
